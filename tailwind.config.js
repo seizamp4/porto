67c4eb11c4af83d658f57b544b507b3a37536b8b
Agas Pratama <165492024+tsukihimp4@users.noreply.github.com>
2024-10-26 17:50:26 +0700

    Porto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#2dd4bf',
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+      screen: {
+        '2xl' : '1320px',
+      },
+    },
+  plugins: [],
+  }
+}
